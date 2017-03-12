a3a6a24c5a10b0a73993e7e2094fcfffe100b281
Helly Shah <hellyshah908@gmail.com>
2017-03-12 20:43:32 +0530

    WCF Service to insert data in sql server

---
+++ InsertSqlServerWCF/InsertSqlServerWCF/IService1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace InsertSqlServerWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    
+    [ServiceContract]
+    public interface IService1
+    {             
+
+        // TODO: Add your service operations here
+
+      
+        [OperationContract]
+        void InsertUserDetails(UserDetails userInfo);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    public class UserDetails
+
+    {
+        
+        string username = string.Empty;
+        DateTime timeStamp;
+        int mode=0;
+        int clockstatus=0;
+
+        //[DataMember]
+        //public int ID
+        //{
+        //    get { return id; }
+        //    set { id = value; }
+        //}
+        [DataMember]
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public DateTime TimeStamp
+        {
+            get { return timeStamp; }
+            set { timeStamp = value; }
+        }
+        [DataMember]
+        public int Mode
+        {
+            get { return mode; }
+            set { mode = value; }
+        }
+        [DataMember]
+        public int ClockStatus
+        {
+            get { return clockstatus; }
+            set { clockstatus = value; }
+        }
+
+                
+    }
+    
+   
+}
---
+++ InsertSqlServerWCF/InsertSqlServerWCF/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using System.IO;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace InsertSqlServerWCF
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession, ConcurrencyMode = ConcurrencyMode.Multiple)] 
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    
+    public class Service1 : IService1
+    {
+        public void InsertUserDetails(UserDetails userInfo)
+        {
+            try
+            {
+                
+                //string Message;
+                SqlConnection con = new SqlConnection(@"Data Source=.\TIMEMANAGER;Database=MutexSS;Integrated security = sspi;Trusted_Connection=True;");
+                //& vAccountId & ";Uid=camsUser;Pwd=p0w3rp4ck;");
+                con.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("insert into tblMutexSS(UserName,TimeStamp,Mode,Clockstatus) values(@UserName,@TimeStamp,@Mode,@ClockStatus)", con);
+
+                    //SqlCommand cmd = new SqlCommand("insert into RegistrationTable(UserName,Password,Country,Email) values(@UserName,@Password,@Country,@Email)", con);
+                    //cmd.Parameters.AddWithValue("@ID", userInfo.ID);
+                    cmd.Parameters.AddWithValue("@UserName", userInfo.UserName);
+                    cmd.Parameters.AddWithValue("@TimeStamp", userInfo.TimeStamp);
+                    cmd.Parameters.AddWithValue("@Mode", userInfo.Mode);
+                    cmd.Parameters.AddWithValue("@ClockStatus", userInfo.ClockStatus);
+                    int result = cmd.ExecuteNonQuery();
+                    
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            catch (Exception t)
+            {
+                throw t;
+            }
+        }
+    }
+}
